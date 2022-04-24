c6dbaf2dba301e6005ce22a7ae92cc8d82fe86d4
arkosh <trukan24@gmail.com>
2022-04-24 21:03:16 +0300

    added base templates

--- calc/views.py
+++ calc/views.py
@@ -1,17 +1,18 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.urls import reverse
 
 
 def thermal(request):
     url = reverse('thermal')
     print(url)
-    return HttpResponse('thermal')
+    return render(request, 'calc/thermal.html')
 
 
 def barrel(request):
-    return HttpResponse('barrel')
+    return render(request, 'calc/barrel.html')
 
 
 def forming(request):
-    return HttpResponse('forming')
+    return render(request, 'calc/forming.html')
