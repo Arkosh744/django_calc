2f37331278e6c92957f76cdc5ba2b1a8814143d7
arkosh <trukan24@gmail.com>
2022-05-24 17:57:43 +0300

    heroku changes

--- djangoProject_calc/settings.py
+++ djangoProject_calc/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://calc-sever-django.herokuapp.com/']
 # Application definition
 
 
