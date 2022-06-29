8effa4001b0f31b88dd8222941b6a6b41b2d843e
arkosh <trukan24@gmail.com>
2022-06-29 17:42:46 +0300

    sidemenu done?

--- static/js/menu_button.js
+++ static/js/menu_button.js
@@ -18,12 +18,20 @@ jQuery(document).ready(function () {
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 
-    $(window).resize(function() {
+    $(window).resize(function () {
         // This will fire each time the window is resized:
-        if($(window).width() <= 600) {
+        if ($(window).width() <= 600) {
             $('.header-bar-menu a div').hide();
         } else {
             $('.header-bar-menu a div').show();
         }
     }).resize(); // This will simulate a resize to trigger the initial run.
+
+    $(window).scroll(function () {
+        $('.sidebar').removeClass('active');
+        $('.overlay').removeClass('active');
+        $('.open-menu').show()
+        $('.dismiss').hide()
+    });
+
 });
